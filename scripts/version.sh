a9e904817c50e6dafe92120ef6a7fb63fabf5240
mpoplavkov <mihail.poplavkov@gmail.com>
2023-09-19 15:04:20 +0200

    fix: adapt version script for mobile only

--- scripts/version.sh
+++ scripts/version.sh
@@ -5,12 +5,12 @@ CURR_VERSION=fuels_beta3-v`awk '/^version: /{print $2}' packages/fuels_beta3/pub
 # iOS & macOS
 APPLE_HEADER="release_tag_name = '$CURR_VERSION' # generated; do not edit"
 sed -i.bak "1 s/.*/$APPLE_HEADER/" packages/flutter_fuels_beta3/ios/flutter_fuels_beta3.podspec
-sed -i.bak "1 s/.*/$APPLE_HEADER/" packages/flutter_fuels_beta3/macos/flutter_fuels_beta3.podspec
-rm packages/flutter_fuels_beta3/macos/*.bak packages/flutter_fuels_beta3/ios/*.bak
+#sed -i.bak "1 s/.*/$APPLE_HEADER/" packages/flutter_fuels_beta3/macos/flutter_fuels_beta3.podspec
+#rm packages/flutter_fuels_beta3/macos/*.bak packages/flutter_fuels_beta3/ios/*.bak
 
 # CMake platforms (Linux, Windows, and Android)
 CMAKE_HEADER="set(LibraryVersion \"$CURR_VERSION\") # generated; do not edit"
-for CMAKE_PLATFORM in android linux windows
+for CMAKE_PLATFORM in android #linux windows
 do
     sed -i.bak "1 s/.*/$CMAKE_HEADER/" packages/flutter_fuels_beta3/$CMAKE_PLATFORM/CMakeLists.txt
     rm packages/flutter_fuels_beta3/$CMAKE_PLATFORM/*.bak
